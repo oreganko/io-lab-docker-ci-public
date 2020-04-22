14083fd51eacd33f6c458f751e45424c9223577f
oregano <organek.natalia@gmail.com>
2020-04-22 11:07:00 +0200

    app and prod

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen by you {} times.\n'.format(count)
